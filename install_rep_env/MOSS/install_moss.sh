3e541a4a9a9341678e487c640735a9fcb84bf6d9
areebol <12657507+areebol@user.noreply.gitee.com>
2023-06-03 23:33:02 +0800

    判断git是否成功

--- install_rep_env/MOSS/install_moss.sh
+++ install_rep_env/MOSS/install_moss.sh
@@ -1,14 +1,25 @@
 #! /bin/bash
+###
+ # @Descripttion: 
+ # @version: 1.0
+ # @Author: Areebol
+ # @Date: 2023-06-03 21:16:04
+ # @LastEditTime: 2023-06-03 23:30:39
+### 
 set -e
 
+if [[ -d 'MOSS' ]]; then
+    cd MOSS
+else 
 # git repo
 git clone https://github.com/OpenLMLab/MOSS.git || {
     echo "git clone to https://github.com/OpenLMLab/MOSS.git failed!"
     exit 1
 }
-
 # enter MOSS
 cd MOSS || exit 1
+fi
+
 
 # create moss
 conda create --name moss python=3.8 || {
