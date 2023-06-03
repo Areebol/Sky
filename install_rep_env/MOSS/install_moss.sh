0adfbf856066fbfe79ca3aa13b43657e7fb7c38f
areebol <12657507+areebol@user.noreply.gitee.com>
2023-06-04 00:05:23 +0800

    增加鲁棒性

--- install_rep_env/MOSS/install_moss.sh
+++ install_rep_env/MOSS/install_moss.sh
@@ -1,36 +1,32 @@
 #! /bin/bash
 ###
- # @Descripttion: 
- # @version: 1.0
- # @Author: Areebol
- # @Date: 2023-06-03 21:16:04
- # @LastEditTime: 2023-06-03 23:30:39
-### 
+# @Descripttion:
+# @version: 1.0
+# @Author: Areebol
+# @Date: 2023-06-03 21:16:04
+ # @LastEditTime: 2023-06-04 00:00:34
+###
 set -e
 
 if [[ -d 'MOSS' ]]; then
     cd MOSS
-else 
-# git repo
-git clone https://github.com/OpenLMLab/MOSS.git || {
-    echo "git clone to https://github.com/OpenLMLab/MOSS.git failed!"
-    exit 1
-}
-# enter MOSS
-cd MOSS || exit 1
+else
+    # git repo
+    git clone https://github.com/OpenLMLab/MOSS.git || {
+        echo "git clone to https://github.com/OpenLMLab/MOSS.git failed!"
+        exit 1
+    }
+    # enter MOSS
+    cd MOSS || exit 1
 fi
 
-
+# remove moss if exits
+conda remove --name moss --all -y || echo "conda path moss do not exit!"
 # create moss
 conda create --name moss python=3.8 || {
     echo "conda create failed!"
     exit 1
 }
-conda activate moss
-echo "activate moss" || {
-    echo "conda create failed!"
-    exit 1
-}
 echo "pip installing requirements.txt ..."
-pip install -r requirements.txt
+conda run -n moss pip install-r requirements.txt
 echo "install finished"
